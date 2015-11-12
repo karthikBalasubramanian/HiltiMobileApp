b1cce6a04df46f0c9697b924cb34513c0a5c1c02
pthejasvi <pthejasv@andrew.cmu.edu>
2015-11-12 21:54:31 +1030

    Removed Volley project folder and included link in dependancies

--- app/src/main/java/quiz/mobile/hiliti/com/hiltimobileapp/constants/UrlEndpoints.java
+++ app/src/main/java/quiz/mobile/hiliti/com/hiltimobileapp/constants/UrlEndpoints.java
@@ -4,9 +4,9 @@ package quiz.mobile.hiliti.com.hiltimobileapp.constants;
  * Created by vaishu on 03-11-2015.
  */
 public class UrlEndpoints {
-        public static String API_SERVER = "http://172.29.80.238:8080";
+        public static String API_SERVER = "http://202.158.217.93:8080";
         public static final String URL_TRAINING = API_SERVER+"/training/viewAlltrainings";
-        public static final String GENERIC_URL_IMAGE_SERVER="http://172.29.80.238:8880/Hilti";
+        public static final String GENERIC_URL_IMAGE_SERVER="http://202.158.217.93:8880/Hilti";
         public static final String LOGIN_URL=API_SERVER+"/userProfile/findEmpByEmailAndPassword";
         public static final String URL_CHAR_QUESTION = "?";
         public static final String URL_CHAR_AMEPERSAND = "&";

0f62ade605638b0c5adecaa88e864cbacd218c9e
karthikB <karthikb@andrew.cmu.edu>
2015-11-26 14:51:58 +1030

    Commiting multiple URL Param Passing. Not yet fixed.

---
+++ app/src/main/java/quiz/mobile/hiliti/com/hiltimobileapp/callbacks/AnsweredCorrectCallBackListener.java
@@ -0,0 +1,12 @@
+package quiz.mobile.hiliti.com.hiltimobileapp.callbacks;
+
+import java.util.ArrayList;
+
+import quiz.mobile.hiliti.com.hiltimobileapp.pojo.TrainingPojo;
+
+/**
+ * Created by vaishu on 26-11-2015.
+ */
+public interface AnsweredCorrectCallBackListener {
+    public void getAnsweredCorrectUpdateResponses(ArrayList<String> answeredCorrectResponse);
+}
